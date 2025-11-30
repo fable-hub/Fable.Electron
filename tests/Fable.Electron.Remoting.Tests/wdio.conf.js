a23129e42bffc630c84749acad63d28dfd9749f9
shayanhabibi <shayanftw@gmail.com>
2025-11-30 11:40:44 +0800

    fix(tests): add retries to tests for spurious fail when not enough time is given for event to occur

--- tests/Fable.Electron.Remoting.Tests/wdio.conf.js
+++ tests/Fable.Electron.Remoting.Tests/wdio.conf.js
@@ -69,7 +69,8 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 60000
+        timeout: 60000,
+        retries: 3
     },
     // =====
     // Hooks
